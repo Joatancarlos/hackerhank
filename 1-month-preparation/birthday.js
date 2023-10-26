ad924427ce3cb8807117020a2b0fea3ccc65eb9c
Joatan Carlos Farias Feitosa <joatancarlos@gmail.com>
2023-10-26 12:08:38 -0300

    birthday

---
+++ 1-month-preparation/birthday.js
@@ -0,0 +1,13 @@
+function birthday(s, d, m) {
+  let ways = 0;
+  for (let i = 0; i < s.length; i++) {
+    const slicePerMonth = s.slice(i, i + m);
+    const sumSquare = slicePerMonth.reduce((acc, curr) => acc + curr);
+    if(sumSquare === d) ways++
+  }
+  return ways;
+}
+console.log(ways);
+console.time()
+birthday([4], 4, 1)
+console.timeEnd()
